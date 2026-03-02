5d4b3e31ba527de63caff356fc9cec7642afe574
Matthew A. Roxas <matthewroxas29@gmail.com>
2026-03-02 09:28:17 +0800

    added ca cert env

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '../generated/prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
+// Added CA_PEM for SSL
 const adapter = new PrismaPg({
   connectionString: connectionString,
   connectionTimeoutMillis: 30000,
